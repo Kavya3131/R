5bc1fe45f7cd7167d7e45a7e5e3b3e6829b5739c
Kongala Kavya (IN67263) <kongala.61067263@ltimindtree.com>
2024-01-19 13:53:50 +0530

    Files added

---
+++ Sample.py
@@ -0,0 +1,12 @@
+x = 5
+y = 10
+z = 15
+
+temp = x
+x = y
+y=z
+z=temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of x after swapping: {}'.format(y))
+print('The value of y after swapping: {}'.format(z))
